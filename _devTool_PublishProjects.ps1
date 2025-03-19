15bb6a9a0da62ccc5477a47c0c098291f1fc5004
MarkusMaal <markus.maal@gmail.com>
2025-03-19 16:01:36 +0200

    devTool patch * Fix exlusion list * Fix out dir for non-mac oses

--- _devTool_PublishProjects.ps1
+++ _devTool_PublishProjects.ps1
@@ -2,13 +2,13 @@
 # Note: Additional files may be created for projects that use LibVLC
 clear
 & "$(Split-Path $MyInvocation.MyCommand.Path)/_devTool_Logo.ps1"
-Get-ChildItem . -Directory -Exclude ".vscode","UniplatformTest" | Foreach-Object {
+Get-ChildItem . -Directory -Exclude ".vscode",".vs",".idea",".git","out","UniplatformTest" | Foreach-Object {
 	# this stores just the directory name, not the full path
 	$pn = $_.Name
 	Write-Output "- Compiling $pn ..."
 	if ($IsMacOS) {
 	    dotnet publish $pn -c Release -o out/$pn -p:PublishReadyToRun=true -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true -p:IncludeNativeLibrariesForSelfExtract=true
 	} else {
-	    dotnet publish $pn -c Release -o $pn -p:PublishReadyToRun=true -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true -p:IncludeNativeLibrariesForSelfExtract=true
+	    dotnet publish $pn -c Release -o out -p:PublishReadyToRun=true -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true -p:IncludeNativeLibrariesForSelfExtract=true
 	}
 }
