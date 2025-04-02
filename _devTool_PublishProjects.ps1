09ec966fe8c0815754cc376515ca5e809895d30e
Markus Maal <markus.maal@gmail.com>
2025-04-03 02:32:00 +0300

    Force x86 compilation on certain LibVLC dependant projects for Apple Silicon

--- _devTool_PublishProjects.ps1
+++ _devTool_PublishProjects.ps1
@@ -2,12 +2,19 @@
 # Note: Additional files may be created for projects that use LibVLC
 clear
 & "$(Split-Path $MyInvocation.MyCommand.Path)/_devTool_Logo.ps1"
+# List of projects that can only be compiled as x86_64 and therefore must be translated through Rosetta when running on macOS
+# We have to do that here, because LibVLC for Apple Silicon isn't available yet
+[string] $RosettaOnly = 'MarkuStation2','Pidu!'
 Get-ChildItem . -Directory -Exclude ".vscode",".vs",".idea",".git","out","UniplatformTest" | Foreach-Object {
 	# this stores just the directory name, not the full path
 	$pn = $_.Name
 	Write-Output "- Compiling $pn ..."
 	if ($IsMacOS) {
-	    dotnet publish $pn -c Release -o out/$pn.d -p:PublishReadyToRun=true -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true -p:IncludeNativeLibrariesForSelfExtract=true -p:DebugSymbols=false
+	    $r_option = ""
+	    if ($RosettaOnly -contains $pn) {
+			$r_option = "-r osx-x64"
+	    }
+	    dotnet publish $pn -c Release $r_option -o out/$pn.d -p:PublishReadyToRun=true -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true -p:IncludeNativeLibrariesForSelfExtract=true -p:DebugSymbols=false
 	} else {
 	    dotnet publish $pn -c Release -o out -p:PublishReadyToRun=true -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true -p:IncludeNativeLibrariesForSelfExtract=true -p:DebugSymbols=false
 	}
